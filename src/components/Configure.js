4406e888674b4d462864b445b0559b80949dcddb
jebitok-dev <sharonjebitok@gmail.com>
2021-08-04 12:42:16 +0300

    Add Images

--- src/components/Configure.js
+++ src/components/Configure.js
@@ -5,17 +5,17 @@ import '../styles/configure.css';
 import { FaRegSun, FaAngleRight } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
-import Welcome1 from '../images/welcome1.jpg';
-import Welcome2 from '../images/welcome2.jpg';
-import Welcome3 from '../images/welcome3.jpg';
-import Welcome4 from '../images/welcome4.jpg';
-import Welcome5 from '../images/welcome5.jpg';
+import Welcome1 from '../images/Welcome1.jpg';
+import Welcome2 from '../images/Welcome2.jpg';
+import Welcome3 from '../images/Welcome3.jpg';
+import Welcome4 from '../images/Welcome4.jpg';
+import Welcome5 from '../images/Welcome5.jpg';
 import Search from '../images/search.png';
 
 const slideImages = [
   Welcome1,
   Welcome2,
-  Welcome4,
+  Welcome3,
   Welcome4,
   Welcome5,
 ];
@@ -27,7 +27,7 @@ class Configure extends React.Component {
       transparent: 'transparent',
       white: 'white',
       oneArray: [0],
-    },
+    };
     this.updateColor = this.updateColor.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
     this.handleRegistration = this.handleRegistration.bind(this);
@@ -48,7 +48,7 @@ class Configure extends React.Component {
   }
 
   updateColor() {
-    const { oneArray } = this.props;
+    const { oneArray } = this.state;
     let item = oneArray[0];
     if (item <= 5) {
       item += 1;
@@ -80,7 +80,66 @@ class Configure extends React.Component {
         );
       })
     );
+
+    return (
+      <div className="wholec">
+        <button type="button" className="hello">
+          <div className="first" />
+          <div className="second" />
+        </button>
+        <div className="loginRegister">
+          <button type="button" className="configure">
+            <FaRegSun className="regsum" />
+            <div className="config">Configure</div>
+            <FaAngleRight className="rightfa" />
+          </button>
+        </div>
+        <div className="search">
+          <img src={Search} alt="" className="simage" />
+        </div>
+        <div className="circlebody">
+          {circle}
+        </div>
+        <div className="slide-container">
+          <Slide>
+            <div className="each-slide">
+              <div className="conffont" style={{ backgroundImage: `url(${slideImages[0]})` }}>
+                <span style={{ color: 'white' }}>VW Golf GTI Clubsport 45</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div className="conffont" style={{ backgroundImage: `url(${slideImages[0]})` }}>
+                <span style={{ color: 'white' }}>VW Golf GTI (Mk7)</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div className="conffont" style={{ backgroundImage: `url(${slideImages[0]})` }}>
+                <span style={{ color: 'white' }}>VW Golf GTI Clubsport</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div className="conffont" style={{ backgroundImage: `url(${slideImages[0]})` }}>
+                <span style={{ color: 'white' }}>Volkswagen Golf 1.0 TSI</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div className="conffont" style={{ backgroundImage: `url(${slideImages[0]})` }}>
+                <span style={{ color: 'white' }}>Volkswagen Golf (Mk7)</span>
+              </div>
+            </div>
+          </Slide>
+        </div>
+      </div>
+    );
   }
 }
+
+Configure.propTypes = {
+  history: PropTypes.string,
+};
+
+Configure.defaultProps = {
+  history: '',
+};
 
 export default Configure;
