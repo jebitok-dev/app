bd7d7c600fa705788afdd3a5949e9cb85d5c99e5
jebitok-dev <sharonjebitok@gmail.com>
2021-08-05 12:46:12 +0300

    Add Changes to Index File

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleWare } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import combinedReducer from './reducers/index';
 import './index.css';
 import App from './components/App';
 
+const store = createStore(combinedReducer, applyMiddleWare(thunk));
+
 ReactDOM.render(
-  <App />, document.getElementById('root'),
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
 );
