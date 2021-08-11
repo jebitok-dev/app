f53a46b21e0051c76046a98462a636ca84209149
jebitok-dev <sharonjebitok@gmail.com>
2021-08-11 15:32:29 +0300

    Add Notification Reducer

---
+++ src/reducers/Notification.js
@@ -0,0 +1,20 @@
+const Notification = (state = {}, action) => {
+  switch (action.type) {
+    case 'SUCCESS':
+      return {
+        type: 'alert-success',
+        message: action.message,
+      };
+    case 'ERROR':
+      return {
+        type: 'alert-danger',
+        message: action.message,
+      };
+    case 'CLEAR':
+      return {};
+    default:
+      return state;
+  }
+};
+
+export default Notification;
