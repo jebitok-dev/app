cc4595a7ac262e31bfb20fb7fe7612384442b4af
jebitok-dev <sharonjebitok@gmail.com>
2021-08-11 16:26:51 +0300

    Add Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,16 @@
 import { combineReducers } from 'redux';
-import getAppointments from './CarReducer';
+import Car from './CarReducer';
+import Authentication from './AuthReducer';
+import Registration from './Registration';
+import Favorite from './FavReducer';
+import Notification from './Notification';
 
-const combinedReducers = combineReducers({ getAppointments });
+const combinedReducers = combineReducers({
+  Authentication,
+  Car,
+  Registration,
+  Favorite,
+  Notification,
+});
 
 export default combinedReducers;
