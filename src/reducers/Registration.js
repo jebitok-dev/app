f82b46a66c44c7bea6a9c0a86faee2577b121223
jebitok-dev <sharonjebitok@gmail.com>
2021-08-11 15:41:04 +0300

    Add Registration Reducer

---
+++ src/reducers/Registration.js
@@ -0,0 +1,16 @@
+const Registration = (state = {}, action) => {
+  switch (action.type) {
+    case 'REGISTER_REQUEST':
+      return {
+        registering: true,
+      };
+    case 'REGISTER_SUCCESS':
+      return {};
+    case 'REGISTER_FAILURE':
+      return {};
+    default:
+      return state;
+  }
+};
+
+export default Registration;
