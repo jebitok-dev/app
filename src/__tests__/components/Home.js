5d9617b6170bfd431a1ee731cd2920a62554e21e
jebitok-dev <sharonjebitok@gmail.com>
2021-08-12 23:03:50 +0300

    Add Home Component Test

---
+++ src/__tests__/components/Home.js
@@ -0,0 +1,18 @@
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router';
+import Home from '../../components/Home';
+import store from '../../index';
+
+describe('Home Component', () => {
+  it('should match snapshot', () => {
+    const home = renderer.create(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Home user={{ name: 'test' }} />
+        </MemoryRouter>
+      </Provider>,
+    ).toJSON();
+    expect(home).toMatchSnapshot();
+  });
+});
